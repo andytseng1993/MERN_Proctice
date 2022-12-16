fd3590d6e50389796de3aaad515ae51db5560750
Andy Tseng <anby1611@gmail.com>
2022-12-16 01:39:55 -0800

    Created crud in items

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const items = require('./routes/api/items')
 
 const app = express()
-
 // BodyParser Middleware
 app.use(bodyParser.json())
 
@@ -13,6 +13,9 @@ const db = require('./config/keys').mongoURI
 mongoose.set("strictQuery", false)
 mongoose.connect(db,()=>console.log('MongoDB Connect...'))
     .catch(err=>console.log(err))
+
+//Use Routes
+app.use('/api/items',items)
 
 const port = process.env.PORT || 3000
 
