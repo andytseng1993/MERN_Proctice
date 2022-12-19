861a64bd821d79a527d491e421a3ef4d90d50bb1
Andy Tseng <anby1611@gmail.com>
2022-12-18 22:19:31 -0800

    Server static assets

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const items = require('./routes/api/items')
+const path = require('path')
 
 const app = express()
 // BodyParser Middleware
@@ -16,6 +17,15 @@ mongoose.connect(db,()=>console.log('MongoDB Connect...'))
 
 //Use Routes
 app.use('/api/items',items)
+
+//Server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 3000
 
