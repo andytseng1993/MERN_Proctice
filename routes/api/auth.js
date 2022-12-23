f7be458a7376c55ac1f4ead58d9b4b3f654bdd5e
Andy Tseng <anby1611@gmail.com>
2022-12-22 21:47:03 -0800

    creted get auth user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 const config = require('config')
 const jwt = require('jsonwebtoken')
+const auth = require('../../middleware/auth')
 
 const User = require('../../models/User')
+const { json } = require('body-parser')
 
 //@route POST api/auth
 //@desc Auth user
@@ -23,7 +25,7 @@ router.post('/',(req,res)=>{
             bcrypt.compare(password,user.password)
                 .then(isMatch=>{
                     if(!isMatch) return res.status(400).json({msg:'Invalid credentials'})
-                    
+
                     jwt.sign({id:user.id},config.get('jwtSecret'),{expiresIn:3600},
                             (err,token)=> {
                                 if(err) throw err
@@ -38,6 +40,15 @@ router.post('/',(req,res)=>{
                             })
                 })
         })
+})
+
+//@route GET api/auth/user, in /austh
+//@desc Auth user
+//@access Public
+router.get('/user', auth ,(req,res)=>{
+    User.findById(req.user.id)
+    .select('-password')
+    .then(user=>res.json(user))
 })
 
 module.exports = router
